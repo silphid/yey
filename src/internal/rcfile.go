3a29fde73a0cec27d8e6dac2f9791f526d1f8b69
davidmdm <desmarais.michaud.david@gmail.com>
2021-05-15 21:15:31 -0400

    abstract rcfile resource

--- src/internal/rcfile.go
+++ src/internal/rcfile.go
@@ -1,10 +1,14 @@
 package yey
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -18,25 +22,20 @@ type RCFile struct {
 	Contexts map[string]Context
 }
 
-func ParseRCFile(wd string) (*Contexts, error) {
+func ParseRCFile(ctx context.Context, resource string) (*Contexts, error) {
 	var rcBytes []byte
 	var err error
-	var yeyrcPath string
 
-	for {
-		yeyrcPath = filepath.Join(wd, ".yeyrc.yaml")
-		rcBytes, err = os.ReadFile(yeyrcPath)
-		if errors.Is(err, os.ErrNotExist) {
-			if wd == "/" {
-				return nil, fmt.Errorf("failed to find .yeyrc.yaml")
-			}
-			wd = filepath.Join(wd, "..")
-			continue
-		}
-		if err != nil {
-			return nil, fmt.Errorf("failed to read .yeyrc.yaml: %w", err)
-		}
-		break
+	if resource == "" {
+		rcBytes, err = readRCFromWorkingDirectory()
+	} else if strings.HasPrefix(resource, "http:") || strings.HasPrefix(resource, "https:") {
+		rcBytes, err = readRCFromNetwork(ctx, resource)
+	} else {
+		rcBytes, err = readRCFromFilepath(resource)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to read rcfile: %w", err)
 	}
 
 	var rcFile RCFile
@@ -56,4 +55,45 @@ func ParseRCFile(wd string) (*Contexts, error) {
 		base:  rcFile.Context,
 		named: rcFile.Contexts,
 	}, nil
+}
+
+func readRCFromWorkingDirectory() ([]byte, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get working directory: %w", err)
+	}
+
+	for {
+		rcBytes, err := os.ReadFile(filepath.Join(wd, ".yeyrc.yaml"))
+		if errors.Is(err, os.ErrNotExist) {
+			if wd == "/" {
+				return nil, fmt.Errorf("failed to find .yeyrc.yaml")
+			}
+			wd = filepath.Join(wd, "..")
+			continue
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to read .yeyrc.yaml: %w", err)
+		}
+		return rcBytes, nil
+	}
+}
+
+func readRCFromFilepath(path string) ([]byte, error) {
+	return os.ReadFile(filepath.Clean(path))
+}
+
+func readRCFromNetwork(ctx context.Context, url string) ([]byte, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to form request: %w", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get response: %w", err)
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
 }
