d3ca43396cd6fe4cc377c94b3820f76a65001e7a
Mathieu Frenette <silphid@users.noreply.github.com>
2021-06-26 07:34:41 -0400

    Output non-essential outputs (prompts, banner...) to stderr

--- src/cmd/prompts.go
+++ src/cmd/prompts.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
 	yey "github.com/silphid/yey/src/internal"
 )
 
@@ -18,9 +20,19 @@ func GetOrPromptContextNames(contexts yey.Contexts, names []string) ([]string, e
 			names = append(names, availableNames[i][0])
 			continue
 		}
+
+		// Render all outputs to stderr
+		renderer := &survey.Renderer{}
+		renderer.WithStdio(terminal.Stdio{
+			In:  os.Stdin,
+			Out: os.Stderr,
+			Err: os.Stderr,
+		})
+
 		prompt := &survey.Select{
-			Message: fmt.Sprintf("Select %s:", contexts.Layers[i].Name),
-			Options: availableNames[i],
+			Message:  fmt.Sprintf("Select %s:", contexts.Layers[i].Name),
+			Renderer: *renderer,
+			Options:  availableNames[i],
 		}
 		selectedIndex := 0
 		if err := survey.AskOne(prompt, &selectedIndex); err != nil {
